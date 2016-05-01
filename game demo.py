4d813db48c25e934efca9fd8ec3fced261a76340
Esmeralda C. Lopez <esmeraldarekz99@gmail.com>
2016-04-30 19:47:55 -0700

    Done

--- game demo.py
+++ game demo.py
@@ -1,17 +1,48 @@
 import random
+import time
+import sys
 
-node = None 
+#INVENTORY
+inventory = []
+
+#TIMER
+sec = 0 
+
+#Into to the game 
+time.sleep(0.5)
+
 
 print 
 print "Welcome to Apoclaiptic City" 
 print   
-#INVENTORY
-inventory = []
+
+node = None 
 
 #INVENTORY
 def addToInventory(item):
     item = raw_input('> what do you want to add?? ')
     inventory.append(item)
+
+#CHARACTERS
+#player status 
+class player(object):
+    
+    def __init__(self, health = 49800, attack = 500):
+        self.attack = attack 
+        self.health = health 
+        
+        
+    def attacks(self, target):
+        target.damage(self.attack)
+        if target.health <= 0:
+            return "Enemy down!"
+        else:
+            return target.health
+            
+    def damage(self, amount):
+        self.health -= amount
+        
+me = player()
 
 #HP OF ZOMBIE
 class Zombie(object):
@@ -59,16 +90,24 @@ class Building:
 
      
 #PATH TO SECOND FLOOR
-Stairs1 = Building("Stairs", 'Pieces from the ceiling are blocking your path. Find another path to get out.\n\nAttack the zombie infornt of you.Head "west"', None, None, None, None, None,'Office1', None, None, None, None)
+Stairs1 = Building("Stairs", 'There is something on you path that is blocking you. Type "flashlight" to see what it is.', None, None, None, None, None,'Office1', None, None, None, None)
 Elevator2 = Building("Second Floor", 'You are now on the second floor. A loud growl is coming for the stairs.... an infected is charging twards you.\n\nHead "south".', 'Elevator', None, None, None, 'Office1', None , None, None, None, None)
      
 #SECOND FLOOR
 Office1 = Building('Office 1', 'There are no items in this room to help you defeat the infected.\nHead "west" into the other room. There might be something in there.', None, None, 'Elevator2', None, None , 'Office2', None, None, None, None)
-Office2 = Building('Office 2 ', 'Huh, nothing in here as well. The infecteds are coming in closer.\nKeep heading "west"', None, None, None, None , None, 'Janitor', None, None, None, None)
+Office2 = Building('Office 2 ', 'Huh, nothing in here as well. The infecteds are coming in closer.\n\nKeep heading "west"', None, None, None, None , None, 'Janitor', None, None, None, None)
 Janitor= Building('Janitor Room', 'Cleaning applicances are scattered everywhere. Within the room there is another door.\n\nWEAPONS\n\nit reads.Type "inside" to get in.', None, None, None, 'Office1', 'Secret',None, None, None, None, 'Secret')
 Secret = Building('Secret Door', 'Great you made it in. Take a step "north"', None, None, 'Weapon', 'Janitor', None,None, None, None, None, None)
 Weapon = Building('Weapon Room', 'A variety of weapons are displayed. Take the ones that you think will be useful. Remember thought there is a limit of 3 weapons you can take.\nType in..\n>add\n\n..to exit and head out Type "east".', None, None, None, 'Elevator3', 'Secret',None, None, None, None, None)
 Bathroom = Building('Restroom', 'The smell of rottening meat is rising in here. Place the first bomb in here. ', None, None, None, None, 'Stairs','Elevator', None, None, None, None)
+
+#PATH TO FIRST FLOOR
+Stairs2 = Building('Stairs', 'Head down as quick and possible', None, 'Lobby', None, None, None, None, None, None, None, None)
+Elevator3 = Building('Elevator', 'Head "down"', None, 'Lobby', None, None, None, None, None, None, None, None)
+
+#FIRST FLOOR
+Lobby = Building('Lobby', 'You are now in the first floor. Head "east"', None, None, None, 'Center', None, None, None, None, None, None)
+Center = Building('Center', 'There is a bomb here. Head "outside" quick!', None, None, None, None, None, None, None, None, 'Enterence', None)
 
 #PATH TO FIRST FLOORt
 node = Stairs1  
@@ -126,7 +165,7 @@ while True:
                     print "Sorry number not in the secret code. \n\n Please try again"
                     print "You have", +turns, 'left'
             if turns == 0:
-                    print "Sorry you lose"#change
+                    print "Sorry try again."#change
                     
                     
          #INVENTORY FOR ITEMS/WEAPONS
@@ -160,7 +199,7 @@ while True:
             if command == "attack":
                 me.health -= zombie.attack 
                 print "Your health is now", me.health 
-                print "You attacked your ene==my for", me.attacks(zombie),"damage"
+                print "You attacked your enemy for", me.attacks(zombie),"damage"
         
 
         
@@ -168,6 +207,7 @@ while True:
                 print
                 print
                 print "Great, you have defeated the zombie!!!\n"
+                print 'Head "west" now.'
         
                 break
         
@@ -176,4 +216,10 @@ while True:
             if me.health <= 0:
         
                 print "Sorry, you died."
-                sys.exit(0)        
+                sys.exit(0)
+                
+    #QUITE THE PROGRAM 
+    if node == Lobby:
+        print "You have reached the end of the demo Apoclaiptic City"
+        sys.exit(0)
+        
